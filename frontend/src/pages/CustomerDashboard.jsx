cd7b366b3b4d10b6ead864a5a384f40bc95bfe56
Aadya <aadyadivyasha@gmail.com>
2026-06-15 10:59:50 +0530

    Add Admin Agent Customer roles

--- frontend/src/pages/CustomerDashboard.jsx
+++ frontend/src/pages/CustomerDashboard.jsx
@@ -19,7 +19,6 @@ const CustomerDashboard = () => {
  const user = JSON.parse(
   localStorage.getItem("user")
 );
-console.log(user);
 
 const customerName =
   user?.name || "";
@@ -36,6 +35,7 @@ const [description, setDescription] = useState("");
       const user = JSON.parse(
   localStorage.getItem("user")
 );
+console.log(user);
 
 const email = user?.email;
       await axios.post(
@@ -62,7 +62,7 @@ const email = user?.email;
   "🎫 Ticket created successfully!"
 );
 
-   
+    setCustomerName("");
 setTitle("");
 setDescription("");
 
@@ -150,6 +150,20 @@ setDescription("");
     }}
   >
     <h2>Create Support Ticket</h2>
+     <input
+  type="text"
+  placeholder="Your Name"
+  value={customerName}
+  onChange={(e) =>
+    setCustomerName(e.target.value)
+  }
+  style={{
+    width: "100%",
+    padding: "12px",
+    marginTop: "15px",
+    marginBottom: "15px",
+  }}
+/>
     <input
       type="text"
       placeholder="Issue Title"
