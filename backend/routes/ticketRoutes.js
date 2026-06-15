8248b2334797910d8129deaf9fda038d561f959b
Aadya <aadyadivyasha@gmail.com>
2026-06-15 13:53:25 +0530

    Add Admin Agent Customer roles

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -5,6 +5,23 @@ const authMiddleware =
 
   const multer =
 require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      Date.now() +
+        "-" +
+        file.originalname
+    );
+  },
+});
+
+const upload = multer({
+  storage,
+});
 const Ticket =
   require("../models/Ticket");
 
